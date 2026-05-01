e4c99a069e85fbd0aedb82afd8d11b616f6cefc0
Stolution <prakash.stolution@gmail.com>
2026-05-01 01:50:53 -0400

    refactor(roulette-backend): add BankrollService (extract financial domain)

---
+++ apps/roulette-backend/src/services/bankroll/index.js
@@ -0,0 +1,26 @@
+const User = require('../../models/User');
+
+const DEFAULT_BANKROLL = 1000;
+
+exports.initBalance = async (userId) => {
+  return User.findByIdAndUpdate(userId, { bankroll: DEFAULT_BANKROLL }, { new: true });
+};
+
+exports.getBalance = async (userId) => {
+  const user = await User.findById(userId).select('bankroll');
+  if (!user) throw new Error('User not found');
+  return user.bankroll;
+};
+
+exports.credit = async (userId, amount) => {
+  if (amount <= 0) throw new Error('Credit amount must be positive');
+  return User.findByIdAndUpdate(userId, { $inc: { bankroll: amount } }, { new: true }).select('bankroll');
+};
+
+exports.debit = async (userId, amount) => {
+  if (amount <= 0) throw new Error('Debit amount must be positive');
+  const user = await User.findById(userId).select('bankroll');
+  if (!user) throw new Error('User not found');
+  if (user.bankroll < amount) throw new Error('Insufficient bankroll');
+  return User.findByIdAndUpdate(userId, { $inc: { bankroll: -amount } }, { new: true }).select('bankroll');
+};
